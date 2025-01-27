7567548c41429c95666934395a32dd9d368d94fe
Similo <171865838+Chief-99@users.noreply.github.com>
2025-01-28 00:09:53 +0200

    Create cells for every row

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,5 +4,11 @@ for (let i = 0; i < 16; i++) {
   let row = document.createElement('div');
   row.classList.add('row');
 
+  for (let j = 0; j < 16; j++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    row.appendChild(cell);
+  }
+
   grid.appendChild(row);
 }
