4fa1f42ea054e9075f438975621824fd00e65ced
Similo <171865838+Chief-99@users.noreply.github.com>
2025-01-27 23:53:08 +0200

    Add 16 div rows to container

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,2 +1,8 @@
 const grid = document.querySelector('#grid-container');
 
+for (let i = 0; i < 16; i++) {
+  let row = document.createElement('div');
+  row.classList.add('row');
+
+  grid.appendChild(row);
+}
